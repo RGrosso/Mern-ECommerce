cd58f60371aa4ede46b307e030e471e896d7d0f3
Ryan Grosso <ryan.grosso@btinternet.com>
2021-06-04 20:35:38 +0100

    Fontawesome, Bootstrap, Footer and header set up

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/cart">
+                                <i class="fas fa-shopping-cart"></i> Cart
+                            </Nav.Link>
+                            <Nav.Link href="/login">
+                                <i class="fas fa-user"></i> Sign In
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
