1032d15689e55d869a48cf7b076060aeec4c2792
Ryan Grosso <ryan.grosso@btinternet.com>
2021-06-29 20:17:26 +0100

    nodemon and concurrently added

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,4 @@ app.get("/api/products/:id", (req, res) => {
     res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+app.listen(5000, console.log("Server running on port 5000..."));
