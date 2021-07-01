384ae3a7c26cf343d03e051e31d9d531a9a91481
Ryan Grosso <ryan.grosso@btinternet.com>
2021-07-01 19:56:43 +0100

    MongoDB and env set up

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import products from "./data/products.js";
+
+dotenv.config();
+
+connectDb();
 
 const app = express();
 
@@ -12,4 +18,5 @@ app.get("/api/products/:id", (req, res) => {
     res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000..."));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
