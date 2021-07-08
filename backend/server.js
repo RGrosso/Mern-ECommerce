bfc8518121cd3da782bfe64ba5773f1f197123e8
Ryan Grosso <ryan.grosso@btinternet.com>
2021-07-08 19:51:40 +0100

    Middleware custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-
 connectDb();
 
 const app = express();
-
-app.get("/api/products", (req, res) => {
-    res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
