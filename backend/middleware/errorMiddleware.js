7751d96f8c1fef8d1d2469e33ef9a4d5f73ebd7f
Ryan Grosso <ryan.grosso@btinternet.com>
2021-07-11 20:05:30 +0100

    Redux store and product reducer set up

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,6 +3,7 @@ const notFound = (req, res, next) => {
     res.status(404);
     next(error);
 };
+
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode);
