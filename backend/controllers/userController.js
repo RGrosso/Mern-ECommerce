ff1b386e4a539fdab6f5831aa2090d608d510c31
Ryan Grosso <ryan.grosso@btinternet.com>
2021-07-19 12:17:54 +0100

    user routes initial set up with jwt auth token

---
+++ backend/controllers/userController.js
@@ -0,0 +1,30 @@
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+import asyncHandler from "express-async-handler";
+
+/**
+ * @description Auth user and get token
+ * @route POST /api/users/login
+ * @access Public
+ */
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user.id),
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password.");
+    }
+
+    res.send({ email, password });
+});
+
+export { authUser };
