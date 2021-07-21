af25742cdfbe4a5173d6fa2a296da12ed7e1a853
Ryan Grosso <ryan.grosso@btinternet.com>
2021-07-21 19:50:44 +0100

    Register route implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,4 +48,38 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 });
 
-export { authUser, getUserProfile };
+/**
+ * @description Registert a new user
+ * @route POST /api/users
+ * @access Public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user.id),
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+});
+
+export { authUser, getUserProfile, registerUser };
